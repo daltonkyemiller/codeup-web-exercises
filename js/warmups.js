47563509df9748e03fd78e43838e47c38a973beb
Dalton Miller <dalton.kye.miller@gmail.com>
2022-04-27 09:28:34 -0500

    completed html exercises and completed warmup 04/27

--- js/warmups.js
+++ js/warmups.js
@@ -1,13 +1,33 @@
 'use strict';
 (function () {
+    // ############################## Warmup 1 ############################## //
+    console.group('Warmup #1');
     console.log('Yay! I completed the warm-up!');
-    // FizzBuzz 1
+    console.groupEnd();
+
+    console.group('Fizzbuzz');
+    // ############################## FizzBuzz Warmup ############################## //
     for (let i = 1; i <= 100; i++) {
         let output = '';
         if (i % 3 === 0) output += 'Fizz';
         if (i % 5 === 0) output += 'Buzz';
         console.log(output !== '' ? output : i);
     }
+    console.groupEnd();
 
-    console.log('-----------------------------');
+
+    // ############################## Warmup 04/27 ############################## //
+    function allFirstLetters(arrOfStr) {
+        // return arrOfStr.map(str => str[0]).join('');
+        let firstLets = '';
+        arrOfStr.forEach(str => firstLets += str[0]);
+        return firstLets;
+    }
+
+    console.group('All First Letters');
+    let arr = ['some', 'string', 'another', 'tring', 'dalton', 'miller'];
+    console.log(`Starting Array: ${arr}`);
+    console.log(`Result: ${allFirstLetters(arr)}`);
+    console.groupEnd();
+
 }());
