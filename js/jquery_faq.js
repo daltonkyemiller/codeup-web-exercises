357b2baf6671d921d74ba5b31e24fcbd3e100f2f
Dalton Miller <dalton.kye.miller@gmail.com>
2022-05-23 16:03:37 -0500

    finished second part of jquery_faq

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -8,4 +8,48 @@ $(() => {
     $('dt').click(function () {
         $(this).toggleClass('highlight');
     });
+
+    $('#last-li-highlight').click(function () {
+        $('#national-parks-detail ul').each(function () {
+            $(this).children().last().toggleClass('highlight');
+        });
+    });
+    $('.national-park-title').click(function () {
+        const nextItem = $(this).next();
+        nextItem.css('font-weight', nextItem.css('font-weight') !== '700' ? '700' : '400');
+    });
+
+    $('#national-parks-detail li').click(function () {
+        $(this).parent().children().first().css('color', 'blue');
+    });
+
+    $('.swap').click(function () {
+        const frameContainer = $(this).parent().parent();
+        const container = $(this).parent();
+        const img = container.find('img');
+        const index = frameContainer.children().index(container);
+        const prevFrame = index === 0
+            ? frameContainer.children().eq(frameContainer.children().length - 1)
+            : frameContainer.children().eq(index - 1);
+
+        const nextFrame = frameContainer.children().eq((index + 1) % frameContainer.children().length);
+        const nextSrc = nextFrame.find('img').attr('src');
+        const prevSrc = prevFrame.find('img').attr('src');
+
+        if (index === 0) {
+            nextFrame.find('img').attr('src', img.attr('src'));
+            img.attr('src', nextSrc);
+        } else if (index === 1) {
+            const switchWith = Math.random() > 0.5
+                ? { frame: nextFrame, src: nextSrc }
+                : { frame: prevFrame, src: prevSrc };
+            switchWith.frame.find('img').attr('src', img.attr('src'));
+            img.attr('src', switchWith.src);
+        } else {
+            prevFrame.find('img').attr('src', img.attr('src'));
+            img.attr('src', prevSrc);
+        }
+
+    });
+
 });
