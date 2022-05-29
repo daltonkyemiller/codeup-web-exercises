a8d137260e94f6e8cd4f85f61844009687485dad
Dalton Miller <dalton.kye.miller@gmail.com>
2022-05-29 15:43:40 -0500

    feat: weather map functionality

--- js/weather_map.js
+++ js/weather_map.js
@@ -19,32 +19,72 @@ const DayCard = (day, temp, desc) => {
     };
 };
 
-const getWeatherData = async () => {
-    const coords = await geocode('San Antonio, TX', MAPBOX_API_KEY);
-    return $.get(OPEN_WEATHER_ENDPOINT, {
-        APPID: OPEN_WEATHER_KEY,
-        lat: coords[1],
-        lon: coords[0],
-        units: 'imperial'
-    });
+const getWeatherData = (location, units) => {
+    return geocode(location, MAPBOX_API_KEY)
+        .then(coords => {
+            return $.get(OPEN_WEATHER_ENDPOINT, {
+                APPID: OPEN_WEATHER_KEY,
+                lat: coords[1],
+                lon: coords[0],
+                units: units
+            })
+                .catch(e => setModal({ title: 'Error retrieving weather data', type: 'error' }));
+        })
+        .catch(e => setModal({ title: 'Can\'t find the location...', type: 'error' }));
 };
 
-console.log(await getWeatherData());
+getWeatherData().then(res => console.log(res));
 
+const setGradient = (date) => {
+    const bgGradients = $('#bg [id*="gradient"]');
+    const dawnGradient = $('#dawn-gradient');
+    const dayGradient = $('#day-gradient');
+    const duskGradient = $('#dusk-gradient');
+    const nightGradient = $('#night-gradient');
+    let currGradient = nightGradient;
+    if (date.getHours() < 12) currGradient = dawnGradient;
+    else if (date.getHours() < 18) currGradient = dayGradient;
+    else if (date.getHours() < 20) currGradient = duskGradient;
 
-const setWeatherCards = async () => {
-    (await getWeatherData()).daily.filter((day, idx) => idx < 6 && idx !== 0).forEach((day) => {
+    currGradient.animate({ opacity: 1 }, 1000);
+    bgGradients.not(currGradient).animate({ opacity: 0 }, 1000);
+    console.log(currGradient);
+
+};
+
+const setDailyWeatherCards = (weatherData) => {
+    let cards = weatherData.daily.filter((day, idx) => idx < 6 && idx !== 0).map((day) => {
         const date = new Date(day.dt * 1000);
-        console.log(day.weather[0].description);
-        $('#forecast #forecast-cards').append($(DayCard(date.toDateString(), day.temp.day, day.weather[0].description).element));
+        return $(DayCard(date.toDateString(), day.temp.day, day.weather[0].description).element);
     });
+
+    $('#forecast-daily-cards').html(cards);
 };
-await setWeatherCards();
+const setCurrentCard = (weatherData, date) => {
+    const time = date.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
+    const todayCard = $('#today-card');
+    let welcomeMsg = 'Good ';
+    if (date.getHours() < 12) welcomeMsg += 'Morning';
+    else if (date.getHours() < 18) welcomeMsg += 'Afternoon';
+    else welcomeMsg += 'Evening';
+    todayCard.find('.time').html(time);
+    todayCard.find('.day').html(date.toLocaleDateString('en-US'));
+    todayCard.find('.temp').html(weatherData.current.temp);
+    todayCard.find('.welcome').html(welcomeMsg);
+};
 
 
-const bgGradients = $('#bg [id*="gradient"]');
-$('.time-change').click(e => {
-    const gradient = $(`#${e.target.value.toLowerCase()}-gradient`);
-    gradient.animate({ opacity: 1 }, 1000);
-    bgGradients.not(gradient).animate({ opacity: 0 }, 1000);
-});
+const renderData = () => {
+    getWeatherData('San Antonio, TX', 'imperial')
+        .then(weatherData => {
+            setDailyWeatherCards(weatherData);
+            setCurrentCard(weatherData, new Date());
+            setGradient(new Date());
+        });
+
+};
+
+renderData();
+setInterval(renderData, 5000);
+
+
